55af5037e14696a67233cc5283623e76a87cc886
Leonardo Silva <leonardosilvalima93@live.com>
2025-03-25 14:34:56 -0300

    recuperando o usuário

--- src/controllers/sessions-controller.ts
+++ src/controllers/sessions-controller.ts
@@ -1,3 +1,6 @@
+import { prisma } from '@/database/prisma'
+import { AppError } from '@/utils/AppError'
+import { compare } from 'bcrypt'
 import { Request, Response } from 'express'
 import { z } from 'zod'
 
@@ -9,6 +12,20 @@ class SessionsController {
     })
 
     const { email, password } = bodySchema.parse(request.body)
+
+    const user = await prisma.user.findFirst({
+      where: { email }
+    })
+
+    if (!user) {
+      throw new AppError('E-mail ou senha inválidos', 401)
+    }
+
+    const passwordMatch = await compare(password, user.password)
+
+    if (!passwordMatch) {
+      throw new AppError('E-mail ou senha inválidos', 401)
+    }
 
     response.json({ email, password })
   }
