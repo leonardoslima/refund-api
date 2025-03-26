682978f193244ef1afe5b06824867119b6a3362e
Leonardo Silva <leonardosilvalima93@live.com>
2025-03-26 14:18:17 -0300

    Implementando paginação

--- src/controllers/refunds-controller.ts
+++ src/controllers/refunds-controller.ts
@@ -38,11 +38,18 @@ class RefundsController {
   async index(request: Request, response: Response) {
     const querySchema = z.object({
       name: z.string().optional().default(""),
+      page: z.coerce.number().optional().default(1),
+      perPage: z.coerce.number().optional().default(10)
     })
 
-    const { name } = querySchema.parse(request.query)
+    const { name, page, perPage } = querySchema.parse(request.query)
+
+    // Calcular os valores de skip
+    const skip = (page - 1) * perPage
 
     const refunds = await prisma.refunds.findMany({
+      skip,
+      take: perPage,
       where: {
         user: {
           name: {
@@ -54,7 +61,28 @@ class RefundsController {
       include: { user: true }
     })
 
-    response.json(refunds)
+    //Obter o total de registros para calcular o número de páginas
+    const totalRecords = await prisma.refunds.count({
+      where: {
+        user: {
+          name: {
+            contains: name.trim(),
+          }
+        }
+      }
+    })
+
+    const totalPages = Math.ceil(totalRecords / perPage)
+
+    response.json({
+      refunds,
+      pagination: {
+        page,
+        perPage,
+        totalRecords,
+        totalPages: totalPages > 0 ? totalPages : 1
+      }
+    })
   }
 }
 
