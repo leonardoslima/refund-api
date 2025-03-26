c3666101bcfe030d2291c2fa36568917cbc5f3b4
Leonardo Silva <leonardosilvalima93@live.com>
2025-03-26 15:18:33 -0300

    exibindo uma solicitação especifica

--- src/routes/refunds-routes.ts
+++ src/routes/refunds-routes.ts
@@ -8,5 +8,6 @@ const refundsController = new RefundsController()
 
 refundsRoutes.post("/", verifyUserAuthorization(["employee"]), refundsController.create)
 refundsRoutes.get("/", verifyUserAuthorization(["manager"]), refundsController.index)
+refundsRoutes.get("/:id", verifyUserAuthorization(["employee", "manager"]), refundsController.show)
 
 export { refundsRoutes }
