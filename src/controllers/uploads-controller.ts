ce4e4088b83c34555dd61e42152d8f5ff1fa2e60
Leonardo Silva <leonardosilvalima93@live.com>
2025-03-27 10:01:59 -0300

    Manipulando arquivo

--- src/controllers/uploads-controller.ts
+++ src/controllers/uploads-controller.ts
@@ -1,10 +1,15 @@
 import { Request, Response } from 'express'
 import z from 'zod'
+import { ZodError } from 'zod'
 
 import uploadConfig from "@/configs/upload"
+import { DiskStorage } from '@/providers/disk-storage'
+import { AppError } from '@/utils/AppError'
 
 class UploadsController {
   async create(request: Request, response: Response) {
+    const diskStorage = new DiskStorage()
+
     try {
       const fileSchema = z.object({
         filename: z
@@ -23,13 +28,23 @@ class UploadsController {
             (size) => size <= uploadConfig.MAX_FILE_SIZE,
             `Arquivo excede o tamanho máximo de ${uploadConfig.MAX_SIZE}MB`
           )
-      }).passthrough()
+      })
+      .passthrough()
 
-      const { file } = fileSchema.parse(request.file)
+      const file = fileSchema.parse(request.file)
+      const filename = await diskStorage.saveFile(file.filename)
 
-      response.json({ message: "ok" })
+      response.json({ filename })
     } catch (error) {
+      if (error instanceof ZodError) {
+        if (request.file) {
+          await diskStorage.deleteFile(request.file.filename, 'tmp')
+        }
 
+        throw new AppError(error.issues[0].message)
+      }
+
+      throw error
     }
   }
 }
